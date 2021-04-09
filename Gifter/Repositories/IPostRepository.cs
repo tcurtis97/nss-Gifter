7b5198bd000fd8a2a5f6a2084333c0f53ad4821b
Tim Curtis <timcurtis97@yahoo.com>
2021-04-09 11:31:39 -0500

    added Ipostrepo and dbutils

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gifter.Models;
+using System.Collections.Generic;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        void Add(Post post);
+        void Delete(int id);
+        List<Post> GetAll();
+        Post GetById(int id);
+        void Update(Post post);
+    }
+}
